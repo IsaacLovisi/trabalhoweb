3e90f4dcf2beae9e1739e777ee0111c13672b1cc
Pedro Calderón <pedrocalderonn52@gmail.com>
2025-11-06 09:12:35 -0300

    fix: corrigida referência do script e melhorias nos textos

--- ex2/script1.js
+++ ex2/script1.js
@@ -1,18 +1,19 @@
 function calcular_media() {
-    let nome = "sem nome"
+    let nome = "sem nome" //inicializa a variável
     nome = document.getElementById("input_nome_aluno").value
 
-    let notas = Array.from(document.getElementsByName("input_nota")).map(nota => parseInt(nota.value))
+    let notas = Array.from(document.getElementsByName("input_nota")).map(nota => parseInt(nota.value)) //pega um array com as notas já em tipo number 
 
     let soma = 0
 
     notas.forEach(nota => {
         soma += nota
-    })
+    }) //soma as notas
 
-    divResultado = document.getElementById("divResultado")
+    divResultado = document.getElementById("resultado-final")
 
-    media = soma / notas.length
+    
+    media = soma / notas.length //calcula media
 
     txtMedia = document.createElement("p")
     txtMedia.innerHTML = `O aluno ${nome} obteve uma média de ${media}, portanto ele obteve o resultado: \n`
@@ -22,8 +23,10 @@ function calcular_media() {
         txtMedia.innerHTML += "⚠️ RECUPERAÇÃO"
     } else {
         txtMedia.innerHTML += "❌ REPROVADO"
-    }
+    } //estrutura de if/else aninhados
         
     divResultado.appendChild(txtMedia)
+    divResultado.style.display = "flex"
+    divResultado.style.flexDirection = "column"
     
 }
